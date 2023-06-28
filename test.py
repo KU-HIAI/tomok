8ab2764c45a2f82cb208829f3732e9fa2fd5c95d
Taemin Lee <persuade@gmail.com>
2023-06-28 13:59:14 +0900

    update test.py

--- test.py
+++ test.py
@@ -4,8 +4,17 @@ class SampleRule(tomok.RuleUnit):
     # [TODO] meta 정보
 
     @tomok.rule_method
-    def example_method(self: tomok.RuleUnit, var1: int, var2: int):
+    def example_method(var1: int, var2: int):
+        """_summary_
+
+        Args:
+            var1 (int): _description_
+            var2 (int): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return var1 * var2
 
 
-SampleRule().example_method(3,4)
+print(SampleRule().example_method(3,4))
