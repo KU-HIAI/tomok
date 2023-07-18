1d024d8f46ce38d06b6042db29d51debaa959d89
주인장 <99159690+jooinjang@users.noreply.github.com>
2023-07-18 21:13:30 +0900

    test files

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import tomok
 
+
 class SampleRuleUnit(tomok.RuleUnit):
     # [TODO] meta 정보
     content = """$z  _{cp} $= 콘크리트 단면의 도심과 프리스트레싱 강재 도심 사이의 거리"""
@@ -17,6 +18,7 @@ class SampleRuleUnit(tomok.RuleUnit):
         """
         return var1 * var2
 
+
 rule_unit = SampleRuleUnit()
-print(rule_unit.example_method(3,4))
+print(rule_unit.example_method(3, 4))
 print(rule_unit.render_markdown())
