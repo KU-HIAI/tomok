a3c280d46a0a5891891fbdd3902cd942feb907d4
Taemin Lee <persuade@gmail.com>
2023-11-09 16:03:29 +0900

    add render_flowchart(), save_flowchart() (#2)

--- tomok/core/rule_unit.py
+++ tomok/core/rule_unit.py
@@ -1,4 +1,8 @@
 import inspect
+import requests
+import json
+import base64
+import zlib
 from .util import typename, import_check
 
 class RuleUnit():
@@ -27,6 +31,56 @@ class RuleUnit():
     def _register_rule_methods(self):
         method_list = [getattr(self, attr) for attr in dir(self) if typename(getattr(self, attr)) == 'tomok.core.decorator.RuleMethod']
         return method_list
+
+
+    def _get_mermaid_ink_url(self, flowchart_str, theme='default'):
+        json_obj = {
+            'code': flowchart_str,
+            'mermaid':{
+                'theme': theme
+            }
+        }
+        pako_str = self._encode_pako(json_obj)
+        url = f"https://mermaid.ink/img/pako:{pako_str}"
+        return url
+
+
+    def _encode_pako(self, json_obj):
+        json_str = json.dumps(json_obj, ensure_ascii=False)
+        decoded_str = bytes(json_str, encoding='utf-8')
+        compressed_data = zlib.compress(decoded_str, level=9)
+        base64_encoded_data = base64.urlsafe_b64encode(compressed_data).decode('utf-8')
+        safe_encoded_str = base64_encoded_data.replace('+', '-').replace('/', '_')
+        safe_encoded_str = safe_encoded_str.rstrip('=')
+
+        return safe_encoded_str
+
+
+    def render_flowchart(self):
+        img_url = self._get_mermaid_ink_url(self.flowchart)
+        from IPython.display import display, Markdown
+        display(Markdown(f"![]({img_url})"))
+
+
+    def save_flowchart(self, filename=None, saved_directory=None):
+        # 파일 이름이 제공되지 않은 경우, 클래스 이름과 '.png' 확장자를 사용합니다.
+        if not filename:
+            class_name = self.__class__.__name__
+            if class_name == '__main__':
+                class_name = 'saved_image'
+            filename = f"{class_name}.png"
+        if saved_directory is not None:
+            import os
+            filename = os.path.join(saved_directory, filename)
+        
+        response = requests.get(self._get_mermaid_ink_url(self.flowchart))
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print(f"이미지가 {filename}으로 저장되었습니다.")
+        else:
+            print("이미지를 다운로드할 수 없습니다.")
+    
 
     def render_markdown(self):
         if not import_check('IPython'):
