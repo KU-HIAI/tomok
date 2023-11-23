eeb4b725a32617dd02632791689e13da79c6ed82
Taemin Lee <persuade@gmail.com>
2023-11-23 15:49:27 +0900

    루트 디렉토리를 import했을 때 shorcut 임시로 추가 (#4)

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import importlib.util
+
+from tomok.core.rule_unit import RuleUnit
+from tomok.core.decorator import rule_method
+
+if importlib.util.find_spec("ifcopenshell"):
+    from tomok.core.rule_controller import RuleController
+    from tomok.ifc.reader import IFCReader
