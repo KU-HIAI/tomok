955c6fe720a5475950889879e4447e1447c74d8d
주인장 <99159690+jooinjang@users.noreply.github.com>
2023-12-12 12:47:42 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@ import importlib.util
 
 from .tomok.core.rule_unit import RuleUnit
 from .tomok.core.decorator import rule_method
-# from .tomok.core.rule_ifc import RuleIFC
 
 from .tomok.core.util import *
 
 if importlib.util.find_spec("ifcopenshell"):
     from .tomok.core.rule_controller import RuleController
     from .tomok.ifc.reader import IFCReader
+    from .tomok.core.rule_ifc import RuleIFC
