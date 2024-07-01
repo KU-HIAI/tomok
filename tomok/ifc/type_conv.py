4637ce7931dea0c97d7ffeab05b01b9ffabea75e
주인장 <99159690+jooinjang@users.noreply.github.com>
2024-07-01 17:30:34 +0900

    Update type_conv.py
    IFCReal 타입 예외처리

--- tomok/ifc/type_conv.py
+++ tomok/ifc/type_conv.py
@@ -1,53 +1,57 @@
 # python
 from enum import IntEnum
+
 # 3rd-party
 from ifcopenshell.file import file
 
 
 def ifc_single_value_to_python(
-    value_type: str,
-    wrapped_value: object,
-    prop: object
+    value_type: str, wrapped_value: object, prop: object
 ) -> object:
     try:
         if wrapped_value is None:
             return None
-        if value_type == 'IfcInteger':
+        if value_type == "IfcInteger":
             return int(wrapped_value)
-        if value_type == 'IfcCountMeasure':
+        if value_type == "IfcCountMeasure":
             return int(wrapped_value)
-        if value_type == 'IfcReal':
-            return float(wrapped_value)
-        if value_type == 'IfcBoolean':
+        if value_type == "IfcReal":
+            if wrapped_value == "":
+                return str(wrapped_value)
+            else:
+                return float(wrapped_value)
+        if value_type == "IfcBoolean":
             return bool(wrapped_value)
-        if value_type == 'IfcLogical':
+        if value_type == "IfcLogical":
             return bool(wrapped_value)
-        if value_type == 'IfcPlaneAngleMeasure':
+        if value_type == "IfcPlaneAngleMeasure":
             return float(wrapped_value)
-        if value_type == 'IfcPositiveLengthMeasure':
+        if value_type == "IfcPositiveLengthMeasure":
             return float(wrapped_value)
-        if value_type == 'IfcLengthMeasure':
+        if value_type == "IfcLengthMeasure":
             return float(wrapped_value)
-        if value_type == 'IfcText':
+        if value_type == "IfcText":
             return str(wrapped_value)
         if type(wrapped_value) is str:
             return wrapped_value
         if type(wrapped_value) is float:
             return float(wrapped_value)
         else:
-            raise TypeError('currently {0} ({1}, {2}) type is not handled in IFCReader.'.format(
-                value_type, wrapped_value, type(wrapped_value)))
+            raise TypeError(
+                "currently {0} ({1}, {2}) type is not handled in IFCReader.".format(
+                    value_type, wrapped_value, type(wrapped_value)
+                )
+            )
     except Exception as ex:
-        raise ValueError('parsing value ({0}) with {1} type is not possible. prop:{2}'.format(
-            wrapped_value, value_type, prop))
+        raise ValueError(
+            "parsing value ({0}) with {1} type is not possible. prop:{2}".format(
+                wrapped_value, value_type, prop
+            )
+        )
 
 
 def python_to_ifc_single_value(
-    ifc: file,
-    name: str,
-    value: object,
-    value_type: str = None,
-    description: str = None
+    ifc: file, name: str, value: object, value_type: str = None, description: str = None
 ):
     new_entity = ifc.createIfcPropertySingleValue()
     new_entity.Name = name
@@ -59,9 +63,7 @@ def python_to_ifc_single_value(
     return new_entity
 
 
-def _recognize_value_type(
-    value: object
-):
+def _recognize_value_type(value: object):
     if type(value) is float:
         return "IfcReal"
     if type(value) is str:
@@ -71,5 +73,8 @@ def _recognize_value_type(
     if type(value) is int:
         return "IfcInteger"
     else:
-        raise TypeError('currently {0} ({1}) type is not writable in IFCReader'.format(
-            type(value), value))
+        raise TypeError(
+            "currently {0} ({1}) type is not writable in IFCReader".format(
+                type(value), value
+            )
+        )
