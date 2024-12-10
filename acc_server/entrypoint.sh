312c6f1421bbb678fb40b10d0e586aaba6b05376
Taemin Lee <taeminlee@korea.ac.kr>
2024-12-10 18:06:11 +0900

    Refactor Docker setup and remove unused files; update dependencies and entry points

--- acc_server/entrypoint.sh
+++ acc_server/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# conda 환경 활성화
-source /opt/conda/etc/profile.d/conda.sh
-conda activate tomok_env
-
 # Extract arguments >> port, 라이브러리 파일 위치, ifc 파일 저장 위치
 PORT=${PORT:-51080}
-LIBRARY_PATH=${LIBRARY_PATH:-/usr/src/app/acc_server/library_files}
-IFC_PATH=${IFC_PATH:-/usr/src/app/acc_server/uploads}
-SPECIFICATION_DIR=${SPECIFICATION_DIR:-/usr/src/app/acc_server/openapi}
+LIBRARY_PATH=${LIBRARY_PATH:-/app/acc_server/library_files}
+IFC_PATH=${IFC_PATH:-/app/acc_server/uploads}
+SPECIFICATION_DIR=${SPECIFICATION_DIR:-/app/acc_server/openapi}
 API_FILE=${API_FILE:-tomok-demo.yaml}
+OPENAPI_URL=${OPENAPI_URL:-https://tomokapi.hiai.kr/v1.0/openapi.json}
 
 # Set environment variables for Docker mount paths
 export LIBRARY_PATH
@@ -20,9 +17,9 @@ export API_FILE
 # Set environment variable for port
 export PORT
 
-export OPENAPI_URL="https://tomokapi.hiai.kr/v1.0/openapi.json"
+export OPENAPI_URL
 
 export HYDRA_FULL_ERROR=1
 
 # Run the acc_server script
-python /usr/src/app/acc_server/app.py
+python /app/acc_server/app.py server.app_port=${PORT}
--- api_server/entrypoint.sh
+++ api_server/entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-# conda 환경 활성화
-source /opt/conda/etc/profile.d/conda.sh
-conda activate tomok_env
-
-PORT=${PORT:-51032}
-
-export PORT
-
-python /usr/src/app/api_server/openapi_generator.py
-python /usr/src/app/api_server/app.py
+python /app/api_server/openapi_generator.py
+python /app/api_server/app.py server.app_port=${PORT:-51032}
