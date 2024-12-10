483d22eb8d22195f6c15b34c722f23118022a2ca
주인장 <leejaewook1333@gmail.com>
2024-12-10 18:41:16 +0900

    임시 저장

--- api_server/run.sh
+++ api_server/run.sh
@@ -1,5 +1,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate tomok
 
+export PORT=51032
+
 python openapi_generator.py
 python app.py
--- temp.sh
+++ temp.sh
@@ -1,11 +1,18 @@
-sudo docker ps -a -q --filter ancestor=tomok_acc | xargs -r sudo docker stop
-sudo docker ps -a -q --filter ancestor=tomok_acc | xargs -r sudo docker rm
-sudo docker rmi tomok_acc
+sudo docker ps -a -q --filter ancestor=acc_server | xargs -r sudo docker stop
+sudo docker ps -a -q --filter ancestor=acc_server | xargs -r sudo docker rm
+sudo docker ps -a -q --filter ancestor=api_server | xargs -r sudo docker stop
+sudo docker ps -a -q --filter ancestor=api_server | xargs -r sudo docker rm
 
-sudo docker build -t tomok_acc .
+sudo docker rmi acc_server
+sudo docker rmi api_server
 
-sudo docker run --network host \
-  -e PORT=51076 \
-  -e SPECIFICATION_DIR=/usr/src/app/acc_server/openapi \
-  -e API_FILE=tomok-demo.yaml \
-  tomok_acc
+
+# sudo docker build -t tomok_acc .
+
+# sudo docker run --network host \
+#   -e PORT=51076 \
+#   -e SPECIFICATION_DIR=/usr/src/app/acc_server/openapi \
+#   -e API_FILE=tomok-demo.yaml \
+#   tomok_acc
+
+sudo docker-compose up --build
